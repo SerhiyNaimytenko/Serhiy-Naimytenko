7803665a150ceee5889e9d4187e65b95ef75449b
Serhiy Naimytenko <s.naimytenko@gmail.com>
2020-02-10 19:21:06 +0200

    Naimytenko S. тест гита

--- task/task 2_1/task 2_1/Source.cpp
+++ task/task 2_1/task 2_1/Source.cpp
@@ -171,7 +171,7 @@ int main()
 
     list_lib.Output();
 
-    printf("Добавления нового елемента\n");
+    printf("Добавления нового елемента\n"); 
     list_lib.list = list_lib.Add();
      
     list_lib.Output();
